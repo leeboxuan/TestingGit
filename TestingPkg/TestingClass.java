b3a9d81d121dda2faffad87cc0a12464697c3605
nathalie.lee <nathalie.lee@WSG18.optimum.net>
2019-08-07 16:30:19 +0800

    Develop Branch

--- TestingPkg/TestingClass.java
+++ TestingPkg/TestingClass.java
@@ -5,12 +5,12 @@ public class TestingClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+
 
 	}
-		void login() {
-			System.out.println("Hello my friends");
-			System.out.println("Version 1");
-
-		}
+	void login() {
+		System.out.println("Hello my friends");
+		System.out.println("Version 1");
+		System.out.println("This is the develop branch");
+	}
 }
