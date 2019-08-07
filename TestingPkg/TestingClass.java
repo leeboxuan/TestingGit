63750063a6daba9859eb3b79d04968e83c4c8d51
nathalie.lee <nathalie.lee@WSG18.optimum.net>
2019-08-07 16:25:47 +0800

    Created Login Method

---
+++ TestingPkg/TestingClass.java
@@ -0,0 +1,16 @@
+package TestingPkg;
+
+public class TestingClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+	}
+		void login() {
+			System.out.println("Hello my friends");
+			System.out.println("Version 1");
+
+		}
+}
