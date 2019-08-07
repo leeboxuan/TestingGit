3706e1e05a8b7f2e2525d290e3e9079a8090ffd5
jerret.sim <jerret.sim@TRAINING010.optimum.net>
2019-08-07 16:50:04 +0800

    updated by developer b

--- TestingPkg/TestingClass.java
+++ TestingPkg/TestingClass.java
@@ -13,4 +13,12 @@ public class TestingClass {
 		System.out.println("Version 1");
 		System.out.println("This is the develop branch");
 	}
+
+	
+	void withdrawMoney() {
+		System.out.println("This is a withdraw method");
+		System.out.println("Version 2");
+		System.out.println("This is the develop branch");
+		System.out.println("Written by developer B");
+	}
 }
